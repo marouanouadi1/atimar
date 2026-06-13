4236240e86ba8939132bd879b0963d9bdb49cf37
Marouan Ouadi <ouadimarouan@gmail.com>
2026-06-13 20:35:29 +0200

    Refactor useThemeColor to ensure default theme is light and update MapPin styles for absolute positioning

--- apps/app/ui/map.tsx
+++ apps/app/ui/map.tsx
@@ -135,7 +135,11 @@ const styles = StyleSheet.create({
     right: "-12%",
   },
   center: {
-    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     alignItems: "center",
     justifyContent: "center",
   },
