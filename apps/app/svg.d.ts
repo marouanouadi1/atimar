e1bfbc698412cf32e6e1790ad1cf8a202c50d52b
Marouan Ouadi <ouadimarouan@gmail.com>
2026-07-12 20:36:50 +0200

    Semplificare logo

---
+++ apps/app/svg.d.ts
@@ -0,0 +1,8 @@
+// Dichiarazioni per gli import di file .svg come componenti React (react-native-svg-transformer).
+declare module "*.svg" {
+  import type { FC } from "react";
+  import type { SvgProps } from "react-native-svg";
+
+  const content: FC<SvgProps>;
+  export default content;
+}
